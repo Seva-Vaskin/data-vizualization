4f3b8bec4bc6147af9779553a6808d31195cceeb
seva-vaskin <vaskin.2003@gmail.com>
2021-10-13 22:52:15 +0300

    add FloatAndStringData parsing

--- src/main/kotlin/Data.kt
+++ src/main/kotlin/Data.kt
@@ -1,8 +1,20 @@
-typealias FloatAndStringData = List<Pair<Float, String>>
+import java.io.File
+
+data class FloatAndStringData(val data: List<Pair<Float, String>>, val sum: Float)
 typealias TwoFloatsData = List<Pair<Float, Float>>
 
 fun parseNumberAndStringData(dataFile: String) : FloatAndStringData {
-    TODO()
+    val resultList = mutableListOf<Pair<Float, String>>()
+    val resultSum = 0f
+    for (line in File(dataFile).readLines()) {
+        val splitLine = line.split('~')
+        if (splitLine.size != 2) {
+            throw Exception("Incorrect data format in line: $line")
+        }
+        val float = splitLine[0].toFloatOrNull() ?: throw Exception("First parameter in line \"$line\" is not a number")
+        resultList.add(Pair(float, splitLine[1]))
+    }
+    return FloatAndStringData(resultList, resultSum)
 }
 
 fun parseTwoFloatsData(dataFile: String) : TwoFloatsData {
