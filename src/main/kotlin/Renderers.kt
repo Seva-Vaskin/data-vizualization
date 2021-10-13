f0a5765c4442022d61a085d242d9b80bd1cfce4a
seva-vaskin <vaskin.2003@gmail.com>
2021-10-13 23:39:40 +0300

    add basic cycle diagram drawer

--- src/main/kotlin/Renderers.kt
+++ src/main/kotlin/Renderers.kt
@@ -1,18 +1,49 @@
 import org.jetbrains.skija.*
 import org.jetbrains.skiko.SkiaLayer
 import org.jetbrains.skiko.SkiaRenderer
+import kotlin.random.Random as Random
 
 abstract class Renderer : SkiaRenderer {
     val typeface = Typeface.makeFromFile("fonts/JetBrainsMono-Regular.ttf")
     val font = Font(typeface, 40f)
-    val paint = Paint().apply {
-        color = Colors.YELLOW.code
-        mode = PaintMode.STROKE
-        strokeWidth = 10f
+
+}
+
+class CycleDiagramRenderer(private val layer: SkiaLayer, val data: NumberAndStringData) : Renderer() {
+    override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
+        val contentScale = layer.contentScale
+        canvas.scale(contentScale, contentScale)
+        val w = (width / contentScale).toInt()
+        val h = (height / contentScale).toInt()
+        val fillPaint = Paint().apply {
+            mode = PaintMode.FILL
+        }
+        val strokePaint = Paint().apply {
+            mode = PaintMode.STROKE
+            color = 0xff000000.toInt()
+            strokeWidth = 5f
+        }
+        val diagramRect = Rect(0f, 0f, w.toFloat(), h.toFloat())
+        val diagramSquare = getMiddleSquare(diagramRect)
+        fillPaint.color = getColorByIndex(0)
+        var prefSum = 0f
+        for (i in data.data.indices) {
+            fillPaint.color = getColorByIndex(i)
+            val startAngle = prefSum / data.sum * 360
+            val sweepAngle = data.data[i].number / data.sum * 360
+            canvas.drawArc(diagramSquare.left, diagramSquare.top, diagramSquare.right, diagramSquare.bottom,
+                startAngle, sweepAngle, true, fillPaint)
+            canvas.drawArc(diagramSquare.left, diagramSquare.top, diagramSquare.right, diagramSquare.bottom,
+                startAngle, sweepAngle, true, strokePaint)
+            prefSum += data.data[i].number
+        }
+        // РИСОВАНИЕ
+        layer.needRedraw()
+
     }
 }
 
-class CycleDiagramRenderer(private val layer: SkiaLayer, val data: FloatAndStringData) : Renderer() {
+class HistogramRenderer(private val layer: SkiaLayer, val data: NumberAndStringData) : Renderer() {
     override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
         val contentScale = layer.contentScale
         canvas.scale(contentScale, contentScale)
@@ -24,15 +55,37 @@ class CycleDiagramRenderer(private val layer: SkiaLayer, val data: FloatAndStrin
     }
 }
 
-class HistogramRenderer(private val layer: SkiaLayer, val data: FloatAndStringData) : Renderer() {
-    override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
-        val contentScale = layer.contentScale
-        canvas.scale(contentScale, contentScale)
-        val w = (width / contentScale).toInt()
-        val h = (height / contentScale).toInt()
-        // РИСОВАНИЕ
-        layer.needRedraw()
-        TODO()
+fun getMiddleSquare(rect: Rect): Rect {
+    val h = rect.height
+    val w = rect.width
+    return if (h < w)
+        Rect(
+            rect.left + (w - h) / 2,
+            rect.top,
+            rect.right - (w - h) / 2,
+            rect.bottom
+        )
+    else
+        Rect(
+            rect.left,
+            rect.top + (h - w) / 2,
+            rect.right,
+            rect.bottom - (h - w) / 2
+        )
+}
+
+fun generateRandomColor(): Int {
+    val r = Random.nextInt() % 256
+    val g = Random.nextInt() % 256
+    val b = Random.nextInt() % 256
+    return ((0xff * 256 + r) * 256 + g) * 256 + b
+}
+
+fun getColorByIndex(index: Int): Int {
+    return if (index <= Colors.values().size) {
+        Colors.values()[index].code
+    } else {
+        generateRandomColor()
     }
 }
 
