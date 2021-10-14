f4d4f79ede984f4ab391786861cec626ef5f3119
seva-vaskin <vaskin.2003@gmail.com>
2021-10-14 17:46:24 +0300

    refactoring: move rectangular transformation functions into a new file

--- src/main/kotlin/Renderers.kt
+++ src/main/kotlin/Renderers.kt
@@ -45,7 +45,7 @@ abstract class Renderer : SkiaRenderer {
             )
             // draw text
             fillPaint.color = 0xff000000.toInt() // black
-            canvas.drawString(legend[i].title, textRect[i].left, textRect[i].bottom , fonts[fontSize.toInt()], fillPaint)
+            canvas.drawString(legend[i].title, textRect[i].left, textRect[i].bottom, fonts[fontSize.toInt()], fillPaint)
         }
     }
 
@@ -116,51 +116,3 @@ class HistogramRenderer(private val layer: SkiaLayer, val data: NumberAndStringD
         TODO()
     }
 }
-
-fun makeRectSmaller(rect: Rect, border: Float): Rect {
-    return Rect(rect.left + border, rect.top + border, rect.right - border, rect.bottom - border)
-}
-
-fun splitRectVertical(rect: Rect, factor: Float): Pair<Rect, Rect> {
-    assert(factor in 0.0..1.0)
-    return Pair(
-        Rect(rect.left, rect.top, rect.right, rect.top + rect.height * factor),
-        Rect(rect.left, rect.top + rect.height * factor, rect.right, rect.bottom)
-    )
-}
-
-fun splitRectHorizontal(rect: Rect, factor: Float): Pair<Rect, Rect> {
-    assert(factor in 0.0..1.0)
-    return Pair(
-        Rect(rect.left, rect.top, rect.left + rect.width * factor, rect.bottom),
-        Rect(rect.left + rect.width * factor, rect.top, rect.right, rect.bottom)
-    )
-}
-
-fun splitRectVerticalIntoBlocks(rect: Rect, count: Int): List<Rect> {
-    val heightOfBlock = rect.height / count
-    return List<Rect>(count) {
-        Rect(rect.left, rect.top + it * heightOfBlock, rect.right, rect.top + (it + 1) * heightOfBlock)
-    }
-}
-
-fun getMiddleSquare(rect: Rect): Rect {
-    val h = rect.height
-    val w = rect.width
-    return if (h < w)
-        Rect(
-            rect.left + (w - h) / 2,
-            rect.top,
-            rect.right - (w - h) / 2,
-            rect.bottom
-        )
-    else
-        Rect(
-            rect.left,
-            rect.top + (h - w) / 2,
-            rect.right,
-            rect.bottom - (h - w) / 2
-        )
-}
-
-
